a9f766e750cadac4e67afdc5aafe8778a5c46d07
David Emelianov <dsemelianov@utexas.edu>
2016-11-09 17:01:23 -0600

    added most of the functions but they still need to be tested

--- database_scripts/db.py
+++ database_scripts/db.py
@@ -47,18 +47,142 @@ def get_playergames_in_daterange(engine, playername, date1, date2, skater = True
     playergames = pd.read_sql_query(sql_query, engine)
     return playergames
 
-
-def get_player_goals_scored(engine, playername, date1, date2):
-	games = get_playergames_in_daterange(engine, playername, date1, date2)
+def get_player_goals_scored_in_season(engine, playername):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
 	goals = games['G'].as_matrix()
 	return goals.sum()
+	
+def get_player_goals_scored_in_past_games(engine, playername, game_number):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
+	goals = games['G'].as_matrix()
+	goals = goals[0:game_number]
+	return goals.sum()
+	
+def get_player_short_handed_goals_scored_in_season(engine, playername):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
+	goals = games['SH'].as_matrix()
+	return goals.sum()
+	
+def get_player_short_handed_goals_scored_in_past_games(engine, playername, game_number):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
+	goals = games['SH'].as_matrix()
+	goals = goals[0:game_number]
+	return goals.sum()
+	
+def get_player_even_handed_goals_scored_in_season(engine, playername):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
+	goals = games['EV'].as_matrix()
+	return goals.sum()
+	
+def get_player_even_handed_goals_scored_in_past_games(engine, playername, game_number):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
+	goals = games['EV'].as_matrix()
+	goals = goals[0:game_number]
+	return goals.sum()
+
+def get_player_power_play_goals_scored_in_season(engine, playername):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
+	goals = games['PP'].as_matrix()
+	return goals.sum()
+	
+def get_player_power_play_goals_scored_in_past_games(engine, playername, game_number):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
+	goals = games['PP'].as_matrix()
+	goals = goals[0:game_number]
+	return goals.sum()
+	
+def get_player_assists_in_season(engine, playername):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
+	goals = games['A'].as_matrix()
+	return goals.sum()
+	
+def get_player_assists_in_past_games(engine, playername, game_number):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
+	goals = games['A'].as_matrix()
+	goals = goals[0:game_number]
+	return goals.sum()
+
+def get_player_plus_minus_in_season(engine, playername):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
+	goals = games['+/-'].as_matrix()
+	return goals.sum()
+	
+def get_player_plus_minus_in_past_games(engine, playername, game_number):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
+	print(games.columns.values)
+	goals = games['+/-'].as_matrix()
+	goals = goals[0:game_number]
+	return goals.sum()
+
+def get_player_shots_in_season(engine, playername):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
+	goals = games['S'].as_matrix()
+	return goals.sum()
+	
+def get_player_shots_in_past_games(engine, playername, game_number):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
+	print(games.columns.values)
+	goals = games['S'].as_matrix()
+	goals = goals[0:game_number]
+	return goals.sum()
+
+def get_player_shot_percentage_in_season(engine, playername):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
+	goals = games['S'].as_matrix()
+	total_shots = goals.sum()
+	##MAKE SURE THAT THIS IS RETURNING THE NUMBER OF ROWS WITHOUT COUNTING THE HEADER
+	total_games = games.shape[0]
+	return total_shots / total_games
+	
+def get_player_shot_percentage_in_past_games(engine, playername, game_number):
+	season_end = datetime.datetime(2016, 10, 12)
+	season_start = datetime.datetime(2015, 10, 7)
+	games = get_playergames_in_daterange(engine, playername, season_start, season_end)
+	print(games.columns.values)
+	goals = games['S'].as_matrix()
+	goals = goals[0:game_number]
+	total_shots = goals.sum()
+	total_games = goals.shape[0]
+	return total_shots / total_games
 
 DB_NAME = "../test.db"
 
 engine = get_engine(DB_NAME)
 
-date1 = datetime.datetime(2015, 10, 7)
-date2 = datetime.datetime(2015, 12, 1)
-goals_scored = get_player_goals_scored(engine, 'Mikael Backlund', date1, date2)
-print(goals_scored)
+#current date
+date2 = datetime.datetime.now()
+#date way in the past
+date1 = datetime.datetime(2000, 12, 1)
+goals = get_player_plus_minus_in_past_games(engine, 'Mikael Backlund', 14)
+print(goals)
 
