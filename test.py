50679c367ba8d229cc15cf089dc90f47224b5921
Matthew Normyle <mmnormyle@gmail.com>
2016-11-15 16:22:48 -0600

    garbage

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
+# import features
 import scraper
+
 season = 2015
-fn_overallgames = scraper.get_overallgames_filename(2015)
-season_url = scraper.SEASON_URLS[season]
-scraper.scrape_games_csv(season_url, fn_overallgames)
+df = scraper.get_raw_overallgames_df(season)
