f73b926de29499ec5c5725e17f081be344a48c5e
Matthew Normyle <mmnormyle@gmail.com>
2016-11-16 03:29:40 -0600

    made test

--- test.py
+++ test.py
@@ -22,24 +22,22 @@ pd.options.display.max_columns = 999
 df_2015 = learning.get_base_data(2015)
 dftrain, Xtrain, ytrain = learning.prepare_base_data(df_2015, "Goals")
 dftest = learning.get_base_data(2014)
-ICD.display(Xtrain)
 
 h = .02  # step size in the mesh
 
-names = ["MLP1", "Nearest Neighbors", "Gaussian Process",
+names = ["MLP1", "Nearest Neighbors", 
          "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
-         "Naive Bayes", "QDA"]
+         "Naive Bayes"]
 
 classifiers = [
     MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(20, 20), random_state=1),
-    KNeighborsClassifier(3),
-    GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),
+    KNeighborsClassifier(1),
     DecisionTreeClassifier(max_depth=5),
     RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
     MLPClassifier(alpha=1),
     AdaBoostClassifier(),
-    GaussianNB(),
-    QuadraticDiscriminantAnalysis()]
+    GaussianNB()]
+    
 
 # iterate over classifiers
 for name, clf in zip(names, classifiers):
